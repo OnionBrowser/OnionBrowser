b5997b4343c65d7ed6260d588ba5b35edd804243
Mike Tigas <mike@tig.as>
2012-02-24 00:56:08 -0800

    base, barebones iOS universal app

---
+++ OnionBrowser/OnionBrowser/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OnionBrowser
+//
+//  Copyright (c) 2012 Mike Tigas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OnionBrowser/OnionBrowser/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OnionBrowser
+//
+//  Copyright (c) 2012 Mike Tigas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
