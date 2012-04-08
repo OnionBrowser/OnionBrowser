3753a8a34a781878c01ecfffffa3c978a32cbc9d
Mike Tigas <mike@tig.as>
2012-04-08 14:02:28 -0700

    compass image is actually BY-SA so my icon is also share-alike

---
+++ OnionBrowser/icon/README.markdown
@@ -0,0 +1,18 @@
+To save space (and not commit large binary files to the repository), icons are
+not stored in this repository.
+
+The full icon set (all sizes required for building) can be downloaded by
+running the "install.sh" script in this directory.
+
+If you are interested in editing the icon, the source file is available:
+
+https://d2p12wh0p3fo1n.cloudfront.net/files/20120408/Icon.pxm
+(6.9M, Pixelmator format)
+
+### License
+
+Available under the Creative Commons BY-SA 3.0 license:
+https://creativecommons.org/licenses/by-sa/3.0/
+
+See LICENSE in repository root for full license information and information
+about original source photographs.
---
+++ OnionBrowser/icon/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pushd "`dirname \"$0\"`"
+curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20120408/Icon-72.png
+curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20120408/Icon-72@2x.png
+curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20120408/Icon-Small-50.png
+curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20120408/Icon-Small-50@2x.png
+curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20120408/Icon-Small.png
+curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20120408/Icon-Small@2x.png
+curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20120408/Icon.png
+curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20120408/Icon@2x.png
+popd
