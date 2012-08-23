d88c2dd56d7ff4ee042d0385df28ae9eddaf2da1
Mike Tigas <mike@tig.as>
2012-08-23 16:34:02 -0700

    1.2.5: remove stray NSLog statement

--- OnionBrowser/ProxyURLProtocol.m
+++ OnionBrowser/ProxyURLProtocol.m
@@ -65,9 +65,6 @@
 
 + (BOOL)canInitWithRequest:(NSURLRequest *)request {
     if ( !([[[request URL] scheme] isEqualToString:@"file"] || [[[request URL] scheme] isEqualToString:@"data"]) ) {
-        if (!([[[request URL] scheme] isEqualToString:@"http"]||[[[request URL] scheme] isEqualToString:@"https"])) {
-            NSLog(@"%@", [[request URL] scheme]);
-        }
         // Previously we checked if it matched "http" or "https". Apparently
         // UIWebView can attempt to make FTP connections for HTML page resources (i.e.
         // a <link> tag for a CSS file with an FTP scheme.). So we whitelist
