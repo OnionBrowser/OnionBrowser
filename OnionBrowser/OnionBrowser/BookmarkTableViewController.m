909ffc0b61359598e61b337c95aba227eea36625
Mike Tigas <mike@tig.as>
2013-03-20 00:27:32 -0400

    finally running a hidden service route to my site

--- OnionBrowser/OnionBrowser/BookmarkTableViewController.m
+++ OnionBrowser/OnionBrowser/BookmarkTableViewController.m
@@ -36,10 +36,11 @@
 - (NSArray *)presetBookmarks {
     if (_presetBookmarks == nil) {
         _presetBookmarks = [NSArray arrayWithObjects:
-                            [NSArray arrayWithObjects:@"Main Site", @"http://onionbrowser.com/", nil],
+                            [NSArray arrayWithObjects:@"Main Site (.onion)", @"http://tigas3l7uusztiqu.onion/onionbrowser/", nil],
+                            [NSArray arrayWithObjects:@"Main Site (HTTPS)", @"https://mike.tig.as/onionbrowser/", nil],
                             [NSArray arrayWithObjects:@"Source Code Site", @"https://github.com/mtigas/iOS-OnionBrowser/", nil],
                             [NSArray arrayWithObjects:@"Version History", @"https://raw.github.com/mtigas/iOS-OnionBrowser/master/CHANGES.txt", nil],
-                            [NSArray arrayWithObjects:@"Mike Tigas, App Developer", @"http://mike.tig.as/", nil],
+                            [NSArray arrayWithObjects:@"Mike Tigas, App Developer", @"http://tigas3l7uusztiqu.onion/", nil],
                             [NSArray arrayWithObjects:@"Tor Bridges", @"https://bridges.torproject.org/", nil],
                              nil];
     }
