99aa7db43bdcdabd2103705825af3fc19798ef8b
Mike Tigas <mike@tig.as>
2013-09-21 17:14:12 -0400

    assets

--- OnionBrowser/icon/install.sh
+++ OnionBrowser/icon/install.sh
@@ -8,4 +8,7 @@ curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20120426/Icon-Small.png
 curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20120426/Icon-Small@2x.png
 curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20120426/Icon.png
 curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20120426/Icon@2x.png
+curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20130921/Icon-120.png
+curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20130921/Icon-152.png
+curl -LO https://d2p12wh0p3fo1n.cloudfront.net/files/20130921/Icon-76.png
 popd
