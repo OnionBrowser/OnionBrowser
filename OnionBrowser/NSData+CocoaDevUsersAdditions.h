b1bd2079f70700a6d7cd3f20d2217d7d37239200
Mike Tigas <mike@tig.as>
2014-05-02 00:24:12 -0400

    remove unused functions from NSData additions

--- OnionBrowser/NSData+CocoaDevUsersAdditions.h
+++ OnionBrowser/NSData+CocoaDevUsersAdditions.h
@@ -14,27 +14,7 @@
 + (NSData *) dataWithBase32String:(NSString *)base32;
 - (NSString *) base32String;
 
-// COBS is an encoding that eliminates 0x00.
-- (NSData *) encodeCOBS;
-- (NSData *) decodeCOBS;
-
-// ZLIB
 - (NSData *) zlibInflate;
-- (NSData *) zlibDeflate;
-
-// GZIP
 - (NSData *) gzipInflate;
-- (NSData *) gzipDeflate;
-
-//CRC32
-- (unsigned int)crc32;
-
-// Hash
-- (NSData*) md5Digest;
-- (NSString*) md5DigestString;
-- (NSData*) sha1Digest;
-- (NSString*) sha1DigestString;
-- (NSData*) ripemd160Digest;
-- (NSString*) ripemd160DigestString;
 
 @end
