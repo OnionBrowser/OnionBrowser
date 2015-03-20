be27b3e0f77d8aec577503c1336216908f130667
Mike Tigas <mike@tig.as>
2015-03-20 17:24:43 -0400

    openssl 1.0.2a

--- build-libssl.sh
+++ build-libssl.sh
@@ -22,7 +22,7 @@
 ###########################################################################
 #  Choose your openssl version and your currently-installed iOS SDK version:
 #
-VERSION="1.0.2"
+VERSION="1.0.2a"
 USERSDKVERSION="8.2"
 MINIOSVERSION="6.1"
 VERIFYGPG=true
