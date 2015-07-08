b9b019850803c40a5d460601ce94084d927fb125
joshua stein <jcs@jcs.org>
2015-07-08 17:37:48 -0500

    call [application ignoreSnapshotOnNextApplicationLaunch] for privacy
    Though reportedly this does not work correctly in some iOS versions,
    but let's try it anyway.

--- Endless/AppDelegate.m
+++ Endless/AppDelegate.m
@@ -31,8 +31,7 @@ NSString *const STATE_RESTORE_TRY_KEY = @"state_restore_lock";
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-	// Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-	// Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+	[application ignoreSnapshotOnNextApplicationLaunch];
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
@@ -48,23 +47,21 @@ NSString *const STATE_RESTORE_TRY_KEY = @"state_restore_lock";
 	}
 	else
 		[[self cookieJar] clearAllOldNonWhitelistedData];
+	
+	[application ignoreSnapshotOnNextApplicationLaunch];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-	// Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 	[[self webViewController] viewIsVisible];
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-	// Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
 	/* this definitely ends our sessions */
 	[[self cookieJar] clearAllNonWhitelistedData];
+	
+	[application ignoreSnapshotOnNextApplicationLaunch];
 }
 
 - (BOOL)application:(UIApplication *)application shouldRestoreApplicationState:(NSCoder *)coder
