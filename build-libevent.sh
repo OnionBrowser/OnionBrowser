8d8fa352bd6c5ab1177405cae4fd47ec9e173035
Mike Tigas <mike@tig.as>
2015-12-25 16:26:30 -0500

    fix libevent download path; it changed

--- build-libevent.sh
+++ build-libevent.sh
@@ -84,8 +84,7 @@ set -e
 
 if [ ! -e "${SRCDIR}/libevent-${VERSION}.tar.gz" ]; then
 	echo "Downloading libevent-${VERSION}.tar.gz"
-	#curl -LO https://github.com/downloads/libevent/libevent/libevent-${VERSION}.tar.gz
-	curl -LO https://sourceforge.net/projects/levent/files/libevent/libevent-2.0/libevent-${VERSION}.tar.gz
+	curl -LO https://github.com/libevent/libevent/releases/download/release-${VERSION}/libevent-${VERSION}.tar.gz
 fi
 echo "Using libevent-${VERSION}.tar.gz"
 
@@ -93,8 +92,7 @@ echo "Using libevent-${VERSION}.tar.gz"
 # may have to import from link on http://www.wangafu.net/~nickm/ or http://www.citi.umich.edu/u/provos/
 if $VERIFYGPG; then
 	if [ ! -e "${SRCDIR}/libevent-${VERSION}.tar.gz.asc" ]; then
-		#curl -LO https://github.com/downloads/libevent/libevent/libevent-${VERSION}.tar.gz.asc
-		curl -LO https://sourceforge.net/projects/levent/files/libevent/libevent-2.0/libevent-${VERSION}.tar.gz.asc
+		curl -LO https://github.com/libevent/libevent/releases/download/release-${VERSION}/libevent-${VERSION}.tar.gz.asc
 	fi
 	echo "Using libevent-${VERSION}.tar.gz.asc"
 	if out=$(gpg --status-fd 1 --verify "libevent-${VERSION}.tar.gz.asc" "libevent-${VERSION}.tar.gz" 2>/dev/null) &&
