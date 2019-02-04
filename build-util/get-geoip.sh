e6c327e3953e99bcef9471c22a502c4da65fb625
Benjamin Erhart <berhart@netzarchitekten.com>
2019-02-04 18:24:58 +0100

    Update to latest Tor.framework version 305.7.2. Improved geoip database fetch script: Only collect if missing or older than a day.

---
+++ build-util/get-geoip.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+#  get-geoip.sh
+#  OnionBrowser2
+#
+#  Created by Benjamin Erhart on 04.02.19.
+#  Copyright © 2019 jcs. All rights reserved.
+
+# Only downloads new geoip files, if they are missing or older than a day.
+
+if [ ! -f ./geoip ] || [ ! -f ./geoip6 ] || test `find . -name geoip -mtime +1`
+then
+    curl -Lo ./geoip https://gitweb.torproject.org/tor.git/plain/src/config/geoip?h=tor-0.3.5.7
+    curl -Lo ./geoip6 https://gitweb.torproject.org/tor.git/plain/src/config/geoip6?h=tor-0.3.5.7
+fi
