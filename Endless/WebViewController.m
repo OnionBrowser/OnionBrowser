030312e8d4d95579379c03db197ed1191ca46578
Benjamin Erhart <berhart@netzarchitekten.com>
2019-03-22 16:51:12 +0100

    Issue #129: Fixed retain cycle warnings.

--- Endless/WebViewController.m
+++ Endless/WebViewController.m
@@ -625,8 +625,10 @@
 
 - (void)addNewTabFromToolbar:(id)_id
 {
+	UITextField *urlField = self->urlField;
+
 	[self addNewTabForURL:nil forRestoration:NO withAnimation:WebViewTabAnimationDefault withCompletionBlock:^(BOOL finished) {
-		[self->urlField becomeFirstResponder];
+		[urlField becomeFirstResponder];
 	}];
 }
 
@@ -724,8 +726,11 @@
 			else {
 				/* no tabs left, add one and zoom out */
 				[self reindexTabs];
+
+				UITextField *urlField = self->urlField;
+
 				[self addNewTabForURL:nil forRestoration:false withAnimation:WebViewTabAnimationDefault withCompletionBlock:^(BOOL finished) {
-					[self->urlField becomeFirstResponder];
+					[urlField becomeFirstResponder];
 				}];
 				return;
 			}
