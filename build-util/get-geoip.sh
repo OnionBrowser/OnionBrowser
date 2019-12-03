762943482d2196e02b1c702837b031817e973775
Benjamin Erhart <berhart@netzarchitekten.com>
2019-12-03 18:37:53 +0100

    Adapted get-geoip.sh script to latest used Tor version.

--- build-util/get-geoip.sh
+++ build-util/get-geoip.sh
@@ -10,6 +10,6 @@
 
 if [ ! -f ./geoip ] || [ ! -f ./geoip6 ] || test `find . -name geoip -mtime +1`
 then
-    curl -Lo ./geoip https://gitweb.torproject.org/tor.git/plain/src/config/geoip?h=tor-0.3.5.8
-    curl -Lo ./geoip6 https://gitweb.torproject.org/tor.git/plain/src/config/geoip6?h=tor-0.3.5.8
+    curl -Lo ./geoip https://gitweb.torproject.org/tor.git/plain/src/config/geoip?h=tor-0.4.0.5
+    curl -Lo ./geoip6 https://gitweb.torproject.org/tor.git/plain/src/config/geoip6?h=tor-0.4.0.5
 fi
